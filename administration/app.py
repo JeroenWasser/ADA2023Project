a72f30814e316fdd755a359fcbd1406ed4a273ff
Jonathan Vandionant <jonathan@MacBook-Pro.local>
2023-05-08 16:06:55 +0200

    Added enum to requirements

--- administration/app.py
+++ administration/app.py
@@ -35,4 +35,4 @@ def get_voting_sessions():
 
 
 if __name__ == '__main__':
-    app.run(port=int(os.environ.get("PORT", 5000)), host='0.0.0.0', debug=True)
+    app.run(port=int(os.environ.get("PORT", 3000)), host='0.0.0.0', debug=True)
