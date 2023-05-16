a9e85176e05f1d7f3960da5f2287fe60023b3485
unknown <jeroenwasser@gmail.com>
2023-05-16 22:17:31 +0200

    managed to reach other service

--- services/administration/resources/party_admin.py
+++ services/administration/resources/party_admin.py
@@ -23,9 +23,20 @@ class PartyAdmin:
     @staticmethod
     def create(p_id, body):
         session = Session()
-        party_admin = PartyAdminDAO(random.randint(5,10000),body['first_name'], body['last_name'], body['status'], body['uuid'], int(p_id), datetime.now(), datetime.now())
+        date_now = datetime.now()
+        party_admin = PartyAdminDAO(random.randint(5,10000),body['first_name'], body['last_name'], body['status'], body['uuid'], int(p_id), date_now, date_now)
         session.add(party_admin)
         session.commit()
         session.refresh(party_admin)
         session.close()
-        return jsonify(party_admin), 200
+        text_out = {
+            "id": party_admin.id,
+            "first_name": party_admin.first_name,
+            "last_name": party_admin.last_name,
+            "status": party_admin.status,
+            "uuid": party_admin.uuid,
+            "party_id": p_id,
+            "created_at": date_now,
+            "edited_at": date_now
+        }
+        return jsonify(text_out), 200
